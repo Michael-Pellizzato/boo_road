a30e743120f57eeb690bfc3d78f7a61467ce0572
Michael-Pellizzato <pellizzato.michael97@gmail.com>
2025-03-24 12:03:23 +0100

    add accordion components

--- src/components/CardTravelers.jsx
+++ src/components/CardTravelers.jsx
@@ -1,57 +1,27 @@
 import traveler from "../data/travelersArray";
-import { useState } from "react";
 
 export default function CardTravelers() {
-  return traveler.map((t) => <TravelerCard key={t.id} traveler={t} />);
-}
-
-function TravelerCard({ traveler }) {
-  const [isOpen, setIsOpen] = useState(false);
-
-  return (
-    <div className="col-md-4 mb-4">
-      <div className="card">
-        {/* Bottone per aprire l'accordion */}
-        <div className="accordion">
-          <div className="accordion-item">
-            <h2 className="accordion-header">
-              <button
-                className={`accordion-button ${isOpen ? "" : "collapsed"}`}
-                type="button"
-                onClick={() => setIsOpen(!isOpen)}
-              >
-                {isOpen
-                  ? `${traveler.nome} ${traveler.cognome}`
-                  : `${traveler.nome} ${traveler.cognome}`}
-              </button>
-            </h2>
-            <div
-              className={`accordion-collapse collapse ${isOpen ? "show" : ""}`}
-            >
-              <div className="accordion-body">
-                <p>
-                  <strong>Data di Nascita:</strong> {traveler.dataNascita}
-                </p>
-                <p>
-                  <strong>CF:</strong> {traveler.codiceFiscale}
-                </p>
-                <p>
-                  <strong>Email:</strong> {traveler.email}
-                </p>
-                <p>
-                  <strong>Telefono:</strong> {traveler.telefono}
-                </p>
-                <p>
-                  <strong>Indirizzo:</strong> {traveler.indirizzo}
-                </p>
-                <p>
-                  <strong>Note:</strong> {traveler.note}
-                </p>
-              </div>
-            </div>
-          </div>
+  return traveler.map((t) => {
+    return (
+      <div key={t.id} className="col-md-4 mb-4">
+        <div className="card">
+          <p>
+            <span>Nome:</span> {t.nome}
+          </p>
+          <p>
+            <span>Cognome:</span> {t.cognome}
+          </p>
+          <p>
+            <span>Data di Nascita:</span> {t.dataNascita}
+          </p>
+          <p>
+            <span>CF:</span> {t.codiceFiscale}
+          </p>
+          <p>
+            <span>Email:</span> {t.email}
+          </p>
         </div>
       </div>
-    </div>
-  );
+    );
+  });
 }
