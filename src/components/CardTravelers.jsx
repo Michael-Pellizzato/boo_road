a3bb8c388bd407032942ad1d8a5aacad4d603374
Michael-Pellizzato <pellizzato.michael97@gmail.com>
2025-03-24 12:03:34 +0100

    1

--- src/components/CardTravelers.jsx
+++ src/components/CardTravelers.jsx
@@ -1,27 +1,6 @@
 import traveler from "../data/travelersArray";
+import AccordionTravelers from "./AccordionTravelers";
 
 export default function CardTravelers() {
-  return traveler.map((t) => {
-    return (
-      <div key={t.id} className="col-md-4 mb-4">
-        <div className="card">
-          <p>
-            <span>Nome:</span> {t.nome}
-          </p>
-          <p>
-            <span>Cognome:</span> {t.cognome}
-          </p>
-          <p>
-            <span>Data di Nascita:</span> {t.dataNascita}
-          </p>
-          <p>
-            <span>CF:</span> {t.codiceFiscale}
-          </p>
-          <p>
-            <span>Email:</span> {t.email}
-          </p>
-        </div>
-      </div>
-    );
-  });
+  return <AccordionTravelers />;
 }
